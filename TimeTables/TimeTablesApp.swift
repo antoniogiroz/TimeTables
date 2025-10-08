8d97951cd831d9093d1bf3853a5f3d5a241c68cd
Antonio Giroz <antonioluisgil@gmail.com>
2025-10-08 07:44:57 +0200

    Initial Commit

---
+++ TimeTables/TimeTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTablesApp.swift
+//  TimeTables
+//
+//  Created by Antonio Giroz on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
